b89f770524624de3ec5b6ea94bf66afd881bfba9
MrTcraft <minecraft123vege@gmail.com>
2023-03-20 00:21:01 -0500

    Open Day
    1ra version del Overviewer de Mundo Mainkra 1.17.1

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -23,11 +23,16 @@ var overviewerConfig = {
         "LOWERRIGHT": 2
     },
     "worlds": [
-        "world"
+        "survival - overworld",
+        "survival - nether",
+        "survival - end",
+        "creative - overworld",
+        "creative - nether",
+        "creative - end"
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1679156978",
+        "cacheTag": "1679289083",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -41,15 +46,15 @@ var overviewerConfig = {
     },
     "tilesets": [
         {
-            "name": "Overviewer Render (lighting)",
-            "zoomLevels": 21,
+            "name": "Survival Daytime",
+            "zoomLevels": 10,
             "defaultZoom": 1,
-            "maxZoom": 21,
-            "path": "world-lighting",
+            "maxZoom": 10,
+            "path": "survivalday",
             "base": "",
             "bgcolor": "#1a1a1a",
-            "world": "world",
-            "last_rendertime": 1679106489,
+            "world": "survival - overworld",
+            "last_rendertime": 1679286568,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
@@ -66,6 +71,182 @@ var overviewerConfig = {
                 70,
                 112
             ],
+            "north_direction": 1
+        },
+        {
+            "name": "Survival Nighttime",
+            "zoomLevels": 10,
+            "defaultZoom": 1,
+            "maxZoom": 10,
+            "path": "survivalnight",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "survival - overworld",
+            "last_rendertime": 1679286568,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                80,
+                70,
+                112
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": [
+                80,
+                70,
+                112
+            ],
+            "north_direction": 1
+        },
+        {
+            "name": "Survival Nether",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "survivalnetherTop",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "survival - nether",
+            "last_rendertime": 1679286485,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                80,
+                70,
+                112
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": false,
+            "north_direction": 0
+        },
+        {
+            "name": "Survival End",
+            "zoomLevels": 8,
+            "defaultZoom": 1,
+            "maxZoom": 8,
+            "path": "survivalEnd",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "survival - end",
+            "last_rendertime": 1679285598,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                80,
+                70,
+                112
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": false,
+            "north_direction": 0
+        },
+        {
+            "name": "Creative Overworld",
+            "zoomLevels": 8,
+            "defaultZoom": 1,
+            "maxZoom": 8,
+            "path": "creativeServer",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "creative - overworld",
+            "last_rendertime": 1679252024,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                0,
+                4,
+                0
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": [
+                0,
+                4,
+                0
+            ],
+            "north_direction": 1
+        },
+        {
+            "name": "Creative Nether",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "CreativeNetherTop",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "creative - nether",
+            "last_rendertime": 1679176811,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                0,
+                4,
+                0
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": false,
+            "north_direction": 0
+        },
+        {
+            "name": "Creative Nether All",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "CreativeNetherAll",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "creative - nether",
+            "last_rendertime": 1679176811,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                0,
+                4,
+                0
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": false,
+            "north_direction": 0
+        },
+        {
+            "name": "Creative End",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "CreativeEnd",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "creative - end",
+            "last_rendertime": 1678928822,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                0,
+                4,
+                0
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": false,
             "north_direction": 0
         }
     ]
